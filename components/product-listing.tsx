c0a43cb5b328844274dc85a17c1c88781cf22201
Praveen <praveen.trainer@outlook.com>
2024-10-26 18:38:14 +0100

    Touchable opacity

--- components/product-listing.tsx
+++ components/product-listing.tsx
@@ -1,7 +1,16 @@
 import { useEffect, useState } from "react";
-import { Text, View, StyleSheet, Button, FlatList, Image } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  FlatList,
+  Image,
+  TouchableHighlight,
+} from "react-native";
 
 import { useRouter } from "expo-router";
+import { TouchableOpacity } from "react-native";
 
 export default function ProductListing() {
   const [products, setProducts] = useState<null | any[]>(null);
@@ -27,29 +36,36 @@ export default function ProductListing() {
       }}
       renderItem={({ item }: any) => {
         return (
-          <View style={Styles.productItem}>
-            <View style={Styles.productImage}>
-              <Text>
-                <Image
-                  width={100}
-                  height={100}
-                  source={{ uri: "https://placehold.co/100x100" }}
-                />
-              </Text>
-            </View>
-            <View style={Styles.productContent}>
-              <Text>{item.title}</Text>
-              <Text>$999</Text>
-            </View>
-            <View style={{ alignSelf: "flex-end" }}>
+          <TouchableOpacity
+            activeOpacity={0.1}
+            onPress={() => {
+              router.navigate(`/products/${item.userId}`);
+            }}
+          >
+            <View style={Styles.productItem}>
+              <View style={Styles.productImage}>
+                <Text>
+                  <Image
+                    width={100}
+                    height={100}
+                    source={{ uri: "https://placehold.co/100x100" }}
+                  />
+                </Text>
+              </View>
+              <View style={Styles.productContent}>
+                <Text>{item.title}</Text>
+                <Text>$999</Text>
+              </View>
+              {/* <View style={{ alignSelf: "flex-end" }}>
               <Button
                 title="Buy now"
                 onPress={() => {
                   router.navigate(`/products/${item.userId}`);
                 }}
               />
+            </View> */}
             </View>
-          </View>
+          </TouchableOpacity>
         );
       }}
     />
